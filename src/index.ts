30b138f52caee5a6fb3e077016d92b39cef3fec6
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-10-07 15:08:25 -0500

    Keyboard command working

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,41 @@ require('dotenv').config();
 import { Markup, Telegraf } from 'telegraf';
 import axios from 'axios';
 
-console.log("hello world")
-
 type ProductStatus = "FULL" | "ONGOING" | "INTERESTING"
 
 type Product = {
-    product_id: string;
-    category_text: string;
-    name: string;
-    apr: string;
-    pol_apr: string;
-    status: ProductStatus;
-    duration: number;
+    product_id: string
+    category_text: string
+    name: string
+    apr: string
+    pol_apr: string
+    status: ProductStatus
+    duration: number
 }
 
 type Currency = {
-    currency: string;
-    products: Product[];
+    currency: string
+    products: Product[]
   };
 
 type KuCoinResponse = {
     data: Currency[];
   };
+
+type Offer = {
+    term: string
+    isAvailable: boolean
+    availability: string
+    apr: number
+    description: string
+    subUrl: string
+}
+
+type CoinOffer = {
+    name: string
+    offers: Offer[]
+}
+
 
 const availabilityMap: { [key in ProductStatus]: string } = {
     'FULL': 'Sold Out',
@@ -32,9 +45,9 @@ const availabilityMap: { [key in ProductStatus]: string } = {
     'INTERESTING': 'Ended'
 }
 
-const coins = ["USDT", "USDC"]
+const coins = ["USDT", "USDC", "SOL", "BNB"]
 
-const getKuCoinData = async () => {
+const getKuCoinData = async (): Promise<CoinOffer[]> => {
     const kuCoinUrl = "https://www.kucoin.com/_pxapi/pool-staking/v3/products/currencies"
     const { data, status } = await axios.get<KuCoinResponse>(kuCoinUrl);
 
@@ -43,7 +56,7 @@ const getKuCoinData = async () => {
     const mapped = data.data
     .filter(coin => coins.includes(coin.currency))
     .map(coin => {
-        const products = coin.products
+        const offers = coin.products
         .filter(p => !isNaN(Number(p.product_id)))
         .map(p => {
             let term
@@ -55,28 +68,46 @@ const getKuCoinData = async () => {
                 case 0: term = 'Flexible'; break;
                 default: term = `${duration} days`
             }
-
-            const totalAPR = Number(apr) + Number(pol_apr)
             // console.log(totalAPR)
 
             const res = {
                 term,
+                isAvailable: status === 'ONGOING',
                 availability: availabilityMap[status],
-                apr: `${totalAPR.toFixed(2)} %`,
+                apr: Number(apr) + Number(pol_apr),
                 description: category_text,
-            }
+                subUrl: 'https://www.kucoin.com/earn'
+            } as Offer
+
             return res
         })
+
         return {
             name: coin.currency,
-            offers: products
+            offers: offers.sort((a, b) => b.apr - a.apr)
         }
     })
 
     return mapped
 }
 
-getKuCoinData().then(console.log)
+const formatReply = (data: CoinOffer[]): string => {
+    let text = ''
+
+    data.forEach(coin => {
+        text += `\n\r<b>${coin.name}</b>\r\n`
+        coin.offers.forEach((offer, idx) => {
+            const { apr, term, description, availability, isAvailable, subUrl } = offer
+            const subLink = isAvailable ? `<a href='${subUrl}'>[Sub]</a>` : ''
+
+            text += `${idx+1} | ${apr.toFixed(2)} % | ${term} | ${description} | ${availability} | ${subLink} \r\n`
+        })
+    })
+
+    return text
+}
+
+// getKuCoinData().then(console.log)
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API_KEY as string);
 
@@ -84,24 +115,28 @@ bot.start((ctx) => ctx.reply('Welcome'));
 
 bot.command('kucoin', async (ctx) => {
     const data = await getKuCoinData()
-    ctx.reply(JSON.stringify(data))
+    
+    ctx.replyWithHTML(formatReply(data))
 })
 
-// bot.on('text', async (ctx) => {
+bot.action('KuCoin', async (ctx) => {
+    const data = await getKuCoinData()
+    
+    ctx.replyWithHTML(formatReply(data))
+})
 
-//     //anything
-//     const { chat } = ctx.message
-//     console.log("id", chat.id)
+bot.on('text', async (ctx) => {
 
-//     ctx.reply(
-//         'Select CEX',
-//         Markup.inlineKeyboard([
-//           Markup.button.callback('KuCoin', 'KuCoin'),
-//         //   Markup.button.callback('Second option', 'second'),
-//         ])
-//       );
+    const { chat } = ctx.message
+    console.log("id", chat.id)
 
-// });
+    ctx.reply(
+        'Select CEX to see the rates',
+        Markup.inlineKeyboard([
+          Markup.button.callback('KuCoin', 'KuCoin'),
+        ])
+      );
+});
 
 
 bot.launch();
